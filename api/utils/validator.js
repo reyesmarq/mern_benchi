7d5d0244b9e9d1e727469667df17bf62831c6e9d
Mau reyes <reyes.marq@gmail.com>
2019-08-02 10:35:07 -0600

    validating information for create_customer

--- api/utils/validator.js
+++ api/utils/validator.js
@@ -4,7 +4,12 @@ const reading = method => {
   switch (method) {
     case 'create_customer':
       return [
-        check('firstName').not().isEmpty().withMessage('First name cannot be empty')
+        check('firstName').not().isEmpty().withMessage('First name cannot be empty'),
+        check('lastName').not().isEmpty().withMessage('Last name cannot be empty'),
+        check('localId').not().isEmpty().withMessage('Local Id cannot be empty'),
+        check('securityCode')
+          .not().isEmpty().withMessage('Security code cannot be empty')
+          .isLength({ min: 4, max: 4 }).withMessage('Security code must be 4 digit long'),
       ]
   }
 }

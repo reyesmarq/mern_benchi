d6f1745029a06de0f9a4ffa934fb5859defa9884
Mau reyes <reyes.marq@gmail.com>
2019-08-21 15:43:47 -0600

    adding swagger express for the api documentation

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,12 @@
 const
   routes = require('express')(),
   customers = require('./customers'),
-  root = require('./root')
+  root = require('./root'),
+  apiUI = require('swagger-ui-express'),
+  apiDocument = require('../config/apiDocumentation.json')
 
 routes
+.use('/doc', apiUI.serve, apiUI.setup(apiDocument, { customCss: '.swagger-ui .topbar { display: none }' }))
 .use('/customers', customers)
 .use('/', root)
 
